76fdf80c3442d2dc61539b9768e32502a065bd41
Posolot <138215725+Posolot@users.noreply.github.com>
2024-05-29 18:48:43 +0800

    Create logger_config.py

---
+++ logger_config.py
@@ -0,0 +1,29 @@
+import logging
+import socket
+
+def setup_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s - %(hostname)s')
+
+    class HostnameFilter(logging.Filter):
+        def filter(self, record):
+            record.hostname = socket.gethostname()
+            return True
+
+    logger.addFilter(HostnameFilter())
+
+    # Console handler
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    # File handler
+    fh = logging.FileHandler('app.log')
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    return logger
